b497bd11232158d0f75253d2b8ccc865783568af
LucaZanella15 <lz2841@cumc.columbia.edu>
2022-01-20 21:38:49 -0500

    pre SA

--- R/GridSearch_pcs_HPC.R
+++ R/GridSearch_pcs_HPC.R
@@ -29,7 +29,7 @@
 #' \code{random.seed}: random seed used to initialize the rng for cell subsamplings. Values are the same as \code{index}.
 #'
 #' @export
-GridSearch_pcs <- function(object,
+GridSearch_pcs_HPC <- function(object,
                                assay.name,
                                .resolutions=seq(0.01,2,by = 0.01),
                                .bootstraps=1,
@@ -50,7 +50,6 @@ GridSearch_pcs <- function(object,
 
   {
 
-
     if (Sys.info()['sysname'] == "Windows"){
       clust.type <- "PSOCK"
     } else if ( (Sys.info()['sysname'] == "Linux") | (Sys.info()['sysname'] == "Darwin") ) {
@@ -58,7 +57,7 @@ GridSearch_pcs <- function(object,
     }
 
     n_cores <- parallel::detectCores()-free.cores
-    myCluster <- parallel::makeCluster(n_cores,type = clust.type)
+    myCluster <- parallel::makeCluster(n_cores,type=clust.type)
     doParallel::registerDoParallel(myCluster)
     n_cells_to_subsample <- round(ncol(object)*.pct_cells/100)
     n_cells_to_subsample
@@ -70,9 +69,11 @@ GridSearch_pcs <- function(object,
            "SLM"=3,
            "Leiden"=4)
 
-
     print(system.time({
-      result <- foreach::foreach( a_res = .resolutions, .combine = 'rbind' ) %dopar% {
+      result <- foreach::foreach( a_res = .resolutions, .combine = 'rbind', .packages=c("dplyr","Seurat","cluster","factoextra") ) %dopar% {
+
+        print(sessionInfo())
+
         knn = rep( .knns , length(.bootstraps) )
         bootstrap = rep( .bootstraps, length(.knns) )
         index <- 1:length(knn)
@@ -81,17 +82,16 @@ GridSearch_pcs <- function(object,
                              tot_sil_neg = 0, lowest_sil_clust = 0, max_sil_clust = 0,
                              sil_avg = 0, sil_mean_median = 0, n_clust = 0, random.seed = 0 )
         nrow(my_sil.df)
+
         for ( a_index in my_sil.df$index )
         {
+
           set.seed(a_index)
           my_sil.df$random.seed[a_index] <- a_index
-          print(paste0("--- Silhouette score computation resolution/bootstrap : ", a_res , "|" , my_sil.df$bootstrap[a_index] ))
           selected_samples <- sample(colnames(object),size=n_cells_to_subsample,replace=.replace)
           x <- object[ , colnames(object) %in% selected_samples ]
-          # x.dist <- as.matrix(vpdist)[selected_samples,selected_samples]
-          # x.dist <- as.matrix(as.dist( 1-cor( x@assays$VIPER@scale.data ,
-          #                                     method = “spe” )))
-          # x.dist <- as.dist(viperSimilarity(x@assays$VIPER@scale.data)) %>% as.matrix()
+
+          print(my_sil.df$knn[a_index])
 
           if (.type=="genes"){
             x.dist <- as.matrix(as.dist( 1-cor( x[[assay.name]]@scale.data,method = "pea" )))
@@ -103,7 +103,7 @@ GridSearch_pcs <- function(object,
                                                   dims=.dims,
                                                   compute.SNN = TRUE)
           } else if (.type=="PCA"){
-            x.dist <- as.matrix(as.dist( 1-cor( t(x@reductions$pca@cell.embeddings),method = "pea" )))
+            x.dist <- as.matrix(as.dist( 1-cor( t(x@reductions$pca@cell.embeddings[,.dims]),method = "pea" )))
             x <- Seurat::FindNeighbors(x,
                                                   reduction = "pca",
                                                   verbose = TRUE,
@@ -124,28 +124,27 @@ GridSearch_pcs <- function(object,
                              algorithm = .clust_alg,
                              random.seed = my_seed
           )
-          table(x$seurat_clusters)
+
           if ( nlevels(x$seurat_clusters) == 1 ) next ;
-          s <- cluster::silhouette( as.integer(x$seurat_clusters) , x.dist )
-          # pdf( file.path(reports.dir,paste0(“sil-res-“,a_res,“.pdf”)) )
-          x <- factoextra::fviz_silhouette(s,print.summary = FALSE)
-          y <- sapply( levels(x$data$cluster) , function(i) mean( x$data$sil_width[ x$data$cluster == i ] ) )
-          z <- sapply( levels(x$data$cluster) , function(i) median( x$data$sil_width[ x$data$cluster == i ] ) )
-          tot_sil_neg <- sapply( levels(x$data$cluster) , function(i) sum( x$data$sil_width[ x$data$cluster == i ] < 0.25 ) )
+          s <- cluster::silhouette( as.integer(x$seurat_clusters), x.dist )
+          x <- factoextra::fviz_silhouette(s)
+          y <- sapply( levels(x$data$cluster), function(i) mean( x$data$sil_width[ x$data$cluster == i ] ) )
+          z <- sapply( levels(x$data$cluster), function(i) median( x$data$sil_width[ x$data$cluster == i ] ) )
+          tot_sil_neg <- sapply( levels(x$data$cluster), function(i) sum( x$data$sil_width[ x$data$cluster == i ] < 0.25 ) )
           my_sil.df$sil_avg[ a_index ] = mean(y)
           my_sil.df$sil_mean_median[ a_index ] = mean(z)
           my_sil.df$tot_sil_neg[ a_index ] = sum(tot_sil_neg)
           my_sil.df$lowest_sil_clust[ a_index ] = min(y)
           my_sil.df$max_sil_clust[a_index] = max(y)
           my_sil.df$n_clust[ a_index ] = nlevels(x$data$cluster)
-          # dev.off()
-          # View(my_sil.df)
+
         }
         my_sil.df$resolution = a_res
         return(my_sil.df)
       } # End of dopar
     })) # End of print
     stopCluster(myCluster)
+
     my_sil.df <- result
 
   }
