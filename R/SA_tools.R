2e868dd4113b3ea1074a7f5c15441e3bdb052106
LucaZanella94 <51045673+LucaZanella94@users.noreply.github.com>
2022-01-31 11:26:22 -0500

    SAClustering

--- R/SA_tools.R
+++ R/SA_tools.R
@@ -60,9 +60,9 @@
 #' @seealso \code{\link[GenSA]{GenSA}}, \code{\link[Seurat]{FindNeighbors}}, \code{\link[Seurat]{FindClusters}}
 #'
 #' @examples
-#' # Do not run
+#' \dontrun{
 #' # Just to retrieve example data
-#' # (devtools::install_github('satijalab/seurat-data') # if package SeuratData is needed, just for e.g.
+#' # devtools::install_github('satijalab/seurat-data') # if package SeuratData is needed, just for e.g.
 #'
 #' library(SeuratData) # just to retrieve some example data
 #' AvailableData() # to see some example data
@@ -75,13 +75,44 @@
 #' clust.optimization <- SAClustering(S.obj=pbmc3k.final,
 #' res.range=c(0.1,1),
 #' NN.range=c(3,15),
-#' verbose=FALSE,
+#' reduction=FALSE,
+#' verbose=TRUE,
 #' final=TRUE,
 #' plot=TRUE)
+#'}
 #'
 #'
-#' # Run SAClustering using principal components as features
-#' clust.optimization <- SAClustering(S.obj=pbmc3.final,
+#' \dontrun{
+#' # Run SAClustering using principal components as features, allowing
+#' # max computation time of 2 min
+#'
+#' # Define control parameters for SAClustering (input to GenSA)
+#' settings <- list(max.time=120) # max.time must be in s
+#'
+#' clust.optimization <- SAClustering(S.obj=pbmc3k.final,
+#' res.range=c(0.1,1),
+#' NN.range=c(3,15),
+#' reduction=TRUE,
+#' control=settings,
+#' plot=FALSE)
+#'}
+#'
+#'
+#'\dontrun{
+#' # Run SAClustering with user-defined optimization settings
+#'
+#' # Increase temperature: computationally, demanding, useful for complex problems
+#'
+#' settings <- list(temperature=1e7)
+#'
+#' clust.optimization <- SAClustering(S.obbj=pbmc3k.final,
+#' res.range=c(0.1,1),
+#' NN.range=c(3,15),
+#' par.init=c(0.5,7),
+#' control=settings
+#' )
+#'}
+#'
 #'
 #'@export
 
@@ -371,7 +402,7 @@ obj.features <- function(x,d,S.obj,NN.range, assay.name, clust.alg, type.fun,ver
 
   par.env$par.history <- rbind(par.env$par.history, c(x[1],NN,num_clusts,obj.fn))
 
-  case(verbose,"TRUE"={cat(c(x[1],NN,num_clusts,obj.fn),"\n")})
+  switch(verbose,"TRUE"={cat(c(x[1],NN,num_clusts,obj.fn),"\n")})
 
 
   return(obj.fn)
@@ -422,7 +453,7 @@ obj.reduction <- function(x,d,S.obj,NN.range, numPCs, assay.name, clust.alg, typ
 
   par.env$par.history <- rbind(par.env$par.history, c(x[1],NN,num_clusts,obj.fn))
 
-  case(verbose, "TRUE"={cat(c(x[1],NN,num_clusts,obj.fn),"\n")})
+  switch(verbose, "TRUE"={cat(c(x[1],NN,num_clusts,obj.fn),"\n")})
 
 
 
